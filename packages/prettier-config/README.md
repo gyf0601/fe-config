dc33ce0fdd37109cc113255d0bdd456457fa56ea
Efe <guoyifei@historage.cn>
2024-02-22 10:40:36 +0800

    feat: add prettier config

---
+++ packages/prettier-config/README.md
@@ -0,0 +1,3 @@
+<h1 align="center">
+  @config/prettier-config
+</h1>
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,9 @@
+export default {
+    tabWidth: 4,
+    printWidth: 120,
+    singleQuote: false,
+    endOfLine: "auto",
+    trailingComma: "all",
+    semi: false,
+    useTabs: false,    
+};
